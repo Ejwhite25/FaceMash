37ab61bb30d8195dc5741f8abd45b5bac0758768
Ejwhite25 <49687191+Ejwhite25@users.noreply.github.com>
2019-12-08 11:15:38 -0700

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -3,7 +3,7 @@
 $user = //database username;
 $password = //database password;
 $database = //name of database;
-$host = "localhost";
+$host = "localhost";//Leave this as is;
 ($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost",/*database username,password,name of database*/));
 mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die ("Unable to select database");
 ?>
