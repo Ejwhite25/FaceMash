9982934f9a0a2870e1257980727c63ada874e223
Ejwhite25 <49687191+Ejwhite25@users.noreply.github.com>
2019-12-08 11:10:39 -0700

    Create redirect.php

---
+++ redirect.php
@@ -0,0 +1,13 @@
+html>
+<body>
+<h1>ERROR 403</h1>
+<?php 
+echo "You do not have permission to view this page";
+?>
+
+
+<a href="https://irrevocable-threats.000webhostapp.com/">Back to Home Page</a>
+
+</body>
+    
+</html>
